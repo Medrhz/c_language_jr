83860f19cce6746eecd77d3b2c85c1ea5311fa0c
Medrhz <sirhziza371@gmail.com>
2025-02-25 11:17:16 +0000

    update condition fle

--- condition.c
+++ condition.c
@@ -3,10 +3,31 @@
 
 void main()
 {
-    /**
 
-      if(condition){
-      }
-     */
-    printf("conditions");
+    // < > <= <=  <>  algo
+    //  < > <= <= != ==    c
+    // et ou not   algo
+    // && ||  !
+    /*
+       Données d'entrée
+       Prix de fabrication : 13
+       Prix de vente : 11
+       Données de sortie
+       Perte
+    */
+    float prixFabrication = 0;
+    float prixVente = 0;
+    printf("Prix de fabrication :\n");
+    scanf("%f", &prixFabrication);
+    printf("Prix de vente   :\n");
+    scanf("%f", &prixVente);
+    float result = prixVente - prixFabrication;
+    if (prixVente < prixFabrication)
+    {
+        printf("Perte : %f", result);
+    }
+    else
+    {
+        printf("profit : %.2f", result);
+    }
 }
