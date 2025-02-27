a97616aa64f31118763e3fedb2bb76e566bfb69b
Medrhz <sirhziza371@gmail.com>
2025-02-27 18:20:05 +0000

    add second example

--- condition.c
+++ condition.c
@@ -15,19 +15,49 @@ void main()
        Données de sortie
        Perte
     */
-    float prixFabrication = 0;
-    float prixVente = 0;
-    printf("Prix de fabrication :\n");
-    scanf("%f", &prixFabrication);
-    printf("Prix de vente   :\n");
-    scanf("%f", &prixVente);
-    float result = prixVente - prixFabrication;
-    if (prixVente < prixFabrication)
+    // float prixFabrication = 0;
+    // float prixVente = 0;
+    // printf("Prix de fabrication :\n");
+    // scanf("%f", &prixFabrication);
+    // printf("Prix de vente   :\n");
+    // scanf("%f", &prixVente);
+    // float result = prixVente - prixFabrication;
+    // if (prixVente < prixFabrication)
+    // {
+    //     printf("Perte : %f", result);
+    // }
+    // else
+    // {
+    //     printf("profit : %.2f", result);
+    // }
+
+    //====== example 2 =========
+    // && => et
+    // || => ou
+    // !  => not
+    int age = 0;
+    int dn = 0;
+    printf("enter dn : \n");
+    scanf("%d", &dn);
+    age = 2025 - dn;
+    if (age >= 6 && age <= 7)
     {
-        printf("Perte : %f", result);
+        printf("Poussin\n");
+    }
+    else if (age >= 8 && age <= 9)
+    {
+        printf("Pupille\n");
+    }
+    else if (age >= 10 && age <= 11)
+    {
+        printf("Minime\n");
+    }
+    else if (age >= 12)
+    {
+        printf("Cadet\n");
     }
     else
     {
-        printf("profit : %.2f", result);
+        printf("ignore\n");
     }
 }
